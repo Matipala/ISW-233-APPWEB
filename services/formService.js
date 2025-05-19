e61e202ab743b680d9592142e624b8cac525adac
Matipala <josue.molina@ucb.edu.bo>
2025-05-19 01:16:26 -0400

    ejercicio-2

--- services/formService.js
+++ services/formService.js
@@ -1,7 +1,7 @@
 let indexItem = 1;
 
-const createform = (content) => {
-    const template = document.getElementById('form__item-template');
+const createtodo = (content) => {
+    const template = document.getElementById('todo__item-template');
     const element = template.content.cloneNode(true).firstElementChild;
     const elementParagraph = element.getElementsByTagName('p')[0];
     elementParagraph.textContent = content;
@@ -10,19 +10,19 @@ const createform = (content) => {
 }
 
 
-const addform = () => {
-    const valueInput = document.getElementById('form-form').firstElementChild.value;
+const addtodo = () => {
+    const valueInput = document.getElementById('todo-todo').firstElementChild.value;
     if (!valueInput || !valueInput.trim() || valueInput.length > 25)
         return;
 
-    const item = createform(valueInput);
+    const item = createtodo(valueInput);
 
 
     item.id = item.id + indexItem.toString();
     indexItem += 1;
 
-    const buttonComplete = item.querySelector('.form__button-complete');
-    const buttonDelete = item.querySelector('.form__button-delete');
+    const buttonComplete = item.querySelector('.todo__button-complete');
+    const buttonDelete = item.querySelector('.todo__button-delete');
 
     buttonComplete.addEventListener('click', () => {
         item.style.opacity = "0.2"
@@ -32,10 +32,10 @@ const addform = () => {
         item.remove();
     })
 
-    const formList = document.getElementById('form__list');
-    formList.appendChild(item);
+    const todoList = document.getElementById('todo__list');
+    todoList.appendChild(item);
 }
 
-document.getElementById('form__button').addEventListener('click', () => {
-    addform();
+document.getElementById('todo__button').addEventListener('click', () => {
+    addtodo();
 });
